4f03c5a095964b72d7be9de3e7176f9515b849d1
ChrisPettigrew <chris@scottishmountainrescue.org>
2020-03-25 11:57:15 +0000

    Progress so far

--- codeclan_homework_chrispettigrew/week_04/cake_ingredients/data_cleaning_scripts/cakes.R
+++ codeclan_homework_chrispettigrew/week_04/cake_ingredients/data_cleaning_scripts/cakes.R
@@ -1,9 +1,12 @@
+# load libraries
 library(tidyverse)
 library(janitor)
 
+# load data
 cakes <- read_csv("raw_data/cake-ingredients-1961.csv")
 codes <- read_csv("raw_data/cake_ingredient_code.csv")
 
+#clean data
 cakes <-
   cakes %>%
   pivot_longer(-Cake, names_to = "code", values_to = "amount") %>%
@@ -12,3 +15,5 @@ cakes <-
   select(-code) %>%
   clean_names()
 
+# write clean data to csv
+write_csv(cakes, 'clean_data/cakes.csv')
